3824cc43a3b57cccf1ef37e0e55609d3f8b996e1
Wildhoney <adam.timberlake@gmail.com>
2015-02-05 20:24:01 +0000

    Added dist build

--- gulpfile.js
+++ gulpfile.js
@@ -5,21 +5,35 @@
         karma   = require('gulp-karma'),
         jshint  = require('gulp-jshint'),
         rename  = require('gulp-rename'),
+        uglify  = require('gulp-uglify'),
         yaml    = require('js-yaml'),
         fs      = require('fs');
 
-    var options = yaml.load(fs.readFileSync('maple.yml'));
+    var options = yaml.load(fs.readFileSync('maple.yml')),
+        files   = ['src/*.js', 'src/components/*.js'];
 
     gulp.task('hint', function gulpHint() {
 
-        return gulp.src(['src/*.js', 'src/components/*.js'])
+        return gulp.src(files)
             .pipe(jshint('.jshintrc'))
             .pipe(jshint.reporter('default'));
 
     });
 
-    gulp.task('test', []);
-    gulp.task('build', []);
+    gulp.task('compile', function gulpCompileJs() {
+
+        return gulp.src(files)
+            .pipe(rename([options.name, 'js'].join('.')))
+            .pipe(gulp.dest('dist'))
+            .pipe(rename([options.name, 'min', 'js'].join('.')))
+            .pipe(uglify())
+            .pipe(gulp.dest('dist'));
+
+    });
+
+
+    gulp.task('test', ['hint']);
+    gulp.task('build', ['compile']);
     gulp.task('default', ['test', 'build']);
 
 })();
