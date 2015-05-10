ac463d31649c2400d9daa57831cf37498d2c52bf
Wildhoney <adam.timberlake@gmail.com>
2015-05-10 23:33:46 +0100

    Modified 'Capybara.app_host'

--- tests/features/support/env.rb
+++ tests/features/support/env.rb
@@ -4,7 +4,7 @@ require 'capybara/dsl'
 require 'capybara/cucumber'
 require 'capybara-webkit'
 Capybara.default_driver = :selenium
-Capybara.app_host = 'http://maple-app.herokuapp.com/'
+Capybara.app_host = 'http://maple-app.herokuapp.com'
 
 #After do |scenario|
 #  save_and_open_page if scenario.failed?
