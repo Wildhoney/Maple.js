173b786b0a58ba97975b978457b10a5972a8dc70
Wildhoney <adam.timberlake@gmail.com>
2015-05-29 11:52:45 +0100

    Added 'npm start' command

--- example/server/default.js
+++ example/server/default.js
@@ -2,11 +2,19 @@
 
     "use strict";
 
-    var express     = require('express'),
-        app         = express(),
-        server      = require('http').createServer(app);
+    var express = require('express'),
+        app     = express(),
+        colour  = require('cli-color'),
+        pad     = require('pad'),
+        server  = require('http').createServer(app),
+        port    = ($process.env.PORT || 5000);
 
     app.use(express.static(__dirname + '/..'));
-    server.listen($process.env.PORT || 5000);
+    server.listen(port);
+
+	var header = colour.xterm(231).bgXterm(161),
+		yellow = colour.xterm(221).bgXterm(232);
+
+    console.log(header(' Maple.js:', yellow(' Running on port ' + port)));
 
 })(process);
